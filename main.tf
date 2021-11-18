55ef3a90e74008cfe6441f96831a1a1c76f04356
aashiqAhamedSP <67874080+aashiqAhamedSP@users.noreply.github.com>
2021-11-18 17:42:42 +0000

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,98 @@
+ terraform {
+  required_version = ">= 0.11" 
+ backend "azurerm" {
+  storage_account_name = "__terraformstorageaccount__"
+    container_name       = "terraform"
+    key                  = "terraform.tfstate"
+	access_key  ="__storagekey__"
+	}
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "3d736b00-44a4-4897-b211-a9178d4b0ed5"
+  client_id       = "5203c43c-dba2-4cd7-9481-055313737721"
+  client_secret   = "oTp7Q~ZCB2CfiL6x8lX2GD4ISzDbE0U9S8FgY"
+  tenant_id       = "9ec05b9b-2f27-413a-a9ae-0a2f2cb9a059"
+}
+
+resource "azurerm_resource_group" "project2_rg" {
+  name     = "__resourcegroupname__"
+  location = "__location__"
+}
+
+resource "azurerm_app_service_plan" "wolkappplan" {
+  name                = "__appserviceplan__"
+  location            = azurerm_resource_group.project2_rg.location
+  resource_group_name = azurerm_resource_group.project2_rg.name
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+resource "azurerm_app_service" "wolkwebapp" {
+  name                = "__appservicename__"
+  location            = azurerm_resource_group.project2_rg.location
+  resource_group_name = azurerm_resource_group.project2_rg.name
+  app_service_plan_id = azurerm_app_service_plan.wolkappplan.id
+}
+
+
+resource "azurerm_storage_account" "wolkwebappstrg22" {
+  name                     = "__logstorage__"
+  resource_group_name      = azurerm_resource_group.project2_rg.name
+  location                 = azurerm_resource_group.project2_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "wolkwebblbcont" {
+  name                  = "wolkwebblbcont"
+  storage_account_name  = azurerm_storage_account.wolkwebappstrg22.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "applogsblob" {
+  name                   = "applogsblob"
+  storage_account_name   = azurerm_storage_account.wolkwebappstrg22.name
+  storage_container_name = azurerm_storage_container.wolkwebblbcont.name
+  type                   = "Block"
+}
+
+data "azurerm_container_registry" "acr" {
+  name                = "__acrname__"
+  resource_group_name = "test"
+}
+
+
+resource "azurerm_kubernetes_cluster" "wolkweb_aks" {
+  name                = "__akscluster__"
+  location            = azurerm_resource_group.project2_rg.location
+  resource_group_name = azurerm_resource_group.project2_rg.name
+  dns_prefix          = "wolkwebaks"
+
+  default_node_pool {
+    name       = "wolknodepl"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+    enable_auto_scaling = false
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+data "azurerm_user_assigned_identity" "aksmi" {
+  name                = "${azurerm_kubernetes_cluster.wolkweb_aks.name}-agentpool"
+  resource_group_name = "MC_${azurerm_resource_group.project2_rg.name}_${azurerm_kubernetes_cluster.wolkweb_aks.name}_${azurerm_resource_group.project2_rg.location}"
+}
+
+resource "azurerm_role_assignment" "role_Contributor" {
+  scope                            = data.azurerm_container_registry.acr.id
+  role_definition_name             = "Contributor"
+  principal_id                     = data.azurerm_user_assigned_identity.aksmi.principal_id
+  skip_service_principal_aad_check = true
+}
